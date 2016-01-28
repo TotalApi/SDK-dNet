319d96db8a7b0bf2b98ce5de7841c0f50955f61c
Sergey Chichulin <re5pawn@users.noreply.github.com>
2016-01-28 14:15:30 +0200

    Update map.js

--- samples/WebApp samples/WebApp/Scripts/app/map.js
+++ samples/WebApp samples/WebApp/Scripts/app/map.js
@@ -24,7 +24,6 @@
         });
 
         // loading and displaying tile layers on the map
-//        L.tileLayer('http://{s}.tile.openstreetmap.se/hydda/full/{z}/{x}/{y}.png', {
         L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
             attribution:
                 'Tiles courtesy of <a href="http://openstreetmap.se/" target="_blank">OpenStreetMap Sweden</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
