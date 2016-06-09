70e7ea14b9c599e600f7079510d43ebc350e64f1
Anton Vdovin <antonvdovin92@gmail.com>
2016-06-09 15:53:47 +0300

    add to sample pipeline of two filters and use loaded custom pipeline

--- samples/SDK samples/DataFilters/SensorDataFilters/Program.cs
+++ samples/SDK samples/DataFilters/SensorDataFilters/Program.cs
@@ -1,17 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
-using TotalApi.Billing;
 using TotalApi.Core;
 using TotalApi.Core.Api;
-using TotalApi.Core.Events;
 using TotalApi.Telematics;
 using TotalApi.Telematics.DataFilters.FilterPipeline;
 using TotalApi.Telematics.DataFilters.SensorsData.Median;
 using TotalApi.Telematics.DataFilters.SensorsData.ThresholdFilter;
-using TotalApi.Utils;
 
 namespace DataFilters
 {
@@ -22,53 +16,62 @@ namespace DataFilters
             TotalApiBootstrapper.Create();
 
             var device = CoreApi.Repository.ExecuteQuery<Device>(q => q.Where(x => x.PhoneNumber == "+555-0005")).FirstOrDefault();
-            //var sensor = CoreApi.Repository.ExecuteQuery<Sensor>(q => q.Where(x => x.DeviceId == device.Id)).FirstOrDefault();
+            var sensor = CoreApi.Repository.ExecuteQuery<Sensor>(q => q.Where(x => x.DeviceId == device.Id)).FirstOrDefault();
 
-            //var medianFilter = new MedianPostFilterParameters {PointsNumber = 11};
-            //var threashold = new SensorDataThresholdPostFilterParameters(280, EqualCondition.Greater, TimeSpan.FromSeconds(1));
+            // Create two filterls
+            var medianFilter = new MedianPostFilterParameters {PointsNumber = 11};
+            var threashold = new SensorDataThresholdPostFilterParameters(280, EqualCondition.Greater, TimeSpan.FromSeconds(1));
 
-
-            //var prms = PipelineSettings.CreateFromFilterParameters(new List<object> { medianFilter });
-            //var pipeLine = TelematicsApi.DataFilterManager.GetSensorDataPostFiltersPipeline(prms);
-
-            //var newCustomPipeline = PipelineSettings.CreateFromFilterParameters(medianFilter, threashold);
-            //var pipelineName = $"Temperature_Sensor_{sensor?.Id}";
+            // Create pipeline
+            var newCustomPipeline = PipelineSettings.CreateFromFilterParameters(medianFilter, threashold);
 
             // Save custom filter to cloud
-            //TelematicsApi.Telematics.SetFiltersPipeline(pipelineName, newCustomPipeline, false, false);
+            var pipelineName = $"Temperature_Sensor_{sensor?.Id}";
+            TelematicsApi.Telematics.SetFiltersPipeline(pipelineName, newCustomPipeline, false, false);
 
-            // Get sensor values without frilters
-            var sensorValues = TelematicsApi.Telematics.ReadSensorValues(new ReadSensorValuesParams
+            if (device != null)
             {
-                SensorType = 0,
-                SensorNumber = 1,
-                DeviceId = device.DeviceId,
-                From = DateTime.Now.AddHours(-5),
-            });
-            Console.WriteLine($"Count values: {sensorValues.Values.Count}");
+                var sensorValuesParams = new ReadSensorValuesParams
+                {
+                    SensorType = 0,
+                    SensorNumber = 1,
+                    DeviceId = device.DeviceId,
+                    From = DateTime.Now.AddHours(-5)
+                };
 
-            // Get sensor value with pipline of one filter
-            var sensorValuesWithFilterPersonal = TelematicsApi.Telematics.ReadSensorValues(new ReadSensorValuesParams
-            {
-                SensorType = 0,
-                SensorNumber = 1,
-                DeviceId = device.DeviceId,
-                From = DateTime.Now.AddHours(-5),
-                Filters = new object[] { new SensorDataThresholdPostFilterParameters(280, EqualCondition.Greater, TimeSpan.FromSeconds(1)) }
-            });
-            Console.WriteLine($"Count values after perosnal filter: {sensorValuesWithFilterPersonal.Values.Count}");
+                // Get sensor values without frilters
+                var sensorValues = TelematicsApi.Telematics.ReadSensorValues(sensorValuesParams);
+            
 
-            /*var sensorValuesWithFilterCustom = TelematicsApi.Telematics.ReadSensorValues(new ReadSensorValuesParams
-            {
-                SensorType = 0,
-                SensorNumber = 1,
-                DeviceId = device.DeviceId,
-                From = DateTime.Now.AddHours(-5),
-                Filters = new object[] { pipelineName }
-            });
-            Console.WriteLine($"Count values after custom filter: {sensorValuesWithFilterCustom.Values.Count}");*/
+                // Get sensor value with pipeline of two filters
+                sensorValuesParams.Filters = new object[]
+                {
+                    // Marks the points by equal condition sensor data values (Greater 280)
+                    new SensorDataThresholdPostFilterParameters(280, EqualCondition.Greater, TimeSpan.FromSeconds(1)),
+                    // Smooths the data
+                    new MedianPostFilterParameters { PointsNumber = 10 }
+                };
+                var sensorValuesWithPersonalPipeline = TelematicsApi.Telematics.ReadSensorValues(sensorValuesParams);
+            
 
-            Console.WriteLine("Press any key to exit");
+                // Use custom filter by name after saving to cloud
+                sensorValuesParams.Filters = new object[] { medianFilter };
+                var sensorValuesWithPipelineCustom = TelematicsApi.Telematics.ReadSensorValues(sensorValuesParams);
+
+
+                // Use custom filter by name after saving to cloud
+                sensorValuesParams.Filters = new object[] { pipelineName };
+                var sensorValuesWithSavedCustimpipeline = TelematicsApi.Telematics.ReadSensorValues(sensorValuesParams);
+
+
+                Console.WriteLine($"Count output values: {sensorValues.Values.Count}");
+                Console.WriteLine($"Count output values after perosnal filter: {sensorValuesWithPersonalPipeline.Values.Count}");
+                Console.WriteLine($"Count output values after custom pipeline: {sensorValuesWithPipelineCustom.Values.Count}");
+                Console.WriteLine($"Count output values after load custom pipeline: {sensorValuesWithSavedCustimpipeline.Values.Count}");
+            }
+
+            var outputInfo = device == null ? "Error: Device not dound\nPress any key to exit" : "Press any key to exit";
+            Console.WriteLine(outputInfo);
             Console.ReadKey();
         }
     }
