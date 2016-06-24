71bd7baee2f7d552ec9d1b928c71e16826f83101
Anton Vdovin <antonvdovin92@gmail.com>
2016-06-24 18:27:54 +0300

    implemented post coordinate pipeline

--- samples/SDK samples/DataFilters/PostFilters/CoordinateDataFilters/Program.cs
+++ samples/SDK samples/DataFilters/PostFilters/CoordinateDataFilters/Program.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using TotalApi.Core;
 using TotalApi.Telematics;
 using TotalApi.Telematics.DataFilters.Coordinates.SimplificateFilter;
+using TotalApi.Telematics.DataFilters.Coordinates.StopsFilter;
 using TotalApi.Telematics.DataFilters.FilterPipeline;
 
 namespace CoordinateDataFilters
@@ -14,20 +15,26 @@ namespace CoordinateDataFilters
             TotalApiBootstrapper.Create();
 
             const double tolerance = 0.00000001;
+            const int minSpeed = 3;
+            const int minStopDurationSeconds = 60;
 
+            var stopfilterParams = new StopsPostFilterParameters { FilterAction = StopsPostFilterParameters.Action.RemoveStops,
+                MinSpeed = minSpeed, MinStopDuration = TimeSpan.FromSeconds(minStopDurationSeconds) };
             var simplificateFilterParams = new SimplificatePostFilterParameters(tolerance);
-            var pipelineSettings = PipelineSettings.CreateFromFilterParameters(simplificateFilterParams);
+            var pipelineSettings = PipelineSettings.CreateFromFilterParameters(simplificateFilterParams, stopfilterParams);
             TelematicsApi.Telematics.SetFiltersPipeline("Name", pipelineSettings, true, false);
 
             var device = TelematicsApi.Telematics.FindDevice(DeviceIdentifier.PhoneNumber("+555-0006"));
             Console.WriteLine($"Current device Id: {device.Id} with OwnId: {device.OwnId}");
 
-            var trackDataWithoutFilters = TelematicsApi.Telematics.ReadCoordinates(device.DeviceId, null, null, "");
+            var trackDataWithoutFilters = TelematicsApi.Telematics.ReadCoordinates(new ReadCoordinatesParams(device.DeviceId));
             Console.WriteLine($"Count coordinates without pipeLine: {trackDataWithoutFilters.Count()}");
 
-            TelematicsApi.Telematics.AssignDefaultCoordinatesPostFiltersPipeline("Name");
-            var trackDataWithFilters = TelematicsApi.Telematics.ReadCoordinates(device.DeviceId, null, null, "");
+            //TelematicsApi.Telematics.AssignDefaultCoordinatesPostFiltersPipeline("Name");
+            var trackDataWithFilters = TelematicsApi.Telematics.ReadCoordinates(device.DeviceId); ;
             Console.WriteLine($"Count coordinates with pipeLine: {trackDataWithFilters.Count()}");
+
+
 
             Console.ReadKey();
         }
