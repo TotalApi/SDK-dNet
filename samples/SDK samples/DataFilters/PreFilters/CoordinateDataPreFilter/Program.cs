1ad69e731f2ed7ac78aac6bebe3101392100a0d4
Anton Vdovin <antonvdovin92@gmail.com>
2016-07-04 16:44:05 +0300

    add coords emulation

--- samples/SDK samples/DataFilters/PreFilters/CoordinateDataPreFilter/Program.cs
+++ samples/SDK samples/DataFilters/PreFilters/CoordinateDataPreFilter/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using TotalApi.Core;
 using TotalApi.Core.Api;
 using TotalApi.Telematics;
@@ -11,7 +8,7 @@ using TotalApi.Telematics.DataFilters.Coordinates.StopsFilter;
 using TotalApi.Telematics.DataFilters.FilterPipeline;
 using TotalApi.Telematics.Events;
 using TotalApi.Utils;
-using TotalApi.Utils.Console;
+using TotalApi.Utils.Spatials;
 
 namespace CoordinateDataPreFilter
 {
@@ -20,6 +17,8 @@ namespace CoordinateDataPreFilter
         private static void Main(string[] args)
         {
             TotalApiBootstrapper.Create();
+
+            var series = CoordinateSourceEmulation.LoadCoordsValues("track1");
 
             const int minSpeed = 3;
             const double tolerance = 0.00000001;
@@ -47,14 +46,18 @@ namespace CoordinateDataPreFilter
             var device = TelematicsApi.Telematics.FindDevice(DeviceIdentifier.PhoneNumber("+555-0006"));
             Console.WriteLine($"Current device Id: {device.Id} with OwnId: {device.OwnId}");
 
-            // Use without pipeline
-            var trackDataWithoutFilters = TelematicsApi.Telematics.ReadCoordinates(new ReadCoordinatesParams(device.DeviceId));
-            Console.WriteLine($"Count coordinates without pipeLine: {trackDataWithoutFilters.Count()}");
+            // Before assign
+            var trackDataWithoutAssign = TelematicsApi.Telematics.ReadCoordinates(new ReadCoordinatesParams(device.DeviceId));
+            Console.WriteLine($"Count coordinates without pipeLine: {trackDataWithoutAssign.Count()}");
 
             TelematicsApi.Telematics.AssignDefaultCoordinatesPreFiltersPipeline(pipeLineName);
 
+            // After assign
+            var trackDataWithAssign = TelematicsApi.Telematics.ReadCoordinates(new ReadCoordinatesParams(device.DeviceId));
+            Console.WriteLine($"Count coordinates without pipeLine: {trackDataWithAssign.Count()}");
 
             Console.WriteLine("Press any key to exit...");
+
             Console.ReadKey();
         }
     }
@@ -67,3 +70,44 @@ namespace CoordinateDataPreFilter
         }
     }
 }
+
+
+/*
+ const int minSpeed = 3;
+            const double tolerance = 0.00000001;
+            const int minStopDurationSeconds = 60;
+            const string pipeLineName = "Default pre coordinate";
+
+            var customEvent = new EventDeviceStateChanged();
+            CoreApi.EventManager.Subscribe(customEvent);
+
+            // Stop filter
+            var stopfilterParams = new StopsPostFilterParameters
+            {
+                FilterAction = StopsPostFilterParameters.Action.RemoveStops,
+                MinSpeed = minSpeed,
+                MinStopDuration = TimeSpan.FromSeconds(minStopDurationSeconds)
+            };
+
+            // Simplificate Filter
+            var simplificateFilterParams = new SimplificatePostFilterParameters(tolerance);
+
+            var pipelineSettings = PipelineSettings.CreateFromFilterParameters(simplificateFilterParams, stopfilterParams);
+
+            TelematicsApi.Telematics.SetFiltersPipeline(pipeLineName, pipelineSettings, isCoordinatesPipeline: true, isPreFiltersPipeline: true);
+
+            var device = TelematicsApi.Telematics.FindDevice(DeviceIdentifier.PhoneNumber("+555-0006"));
+            Console.WriteLine($"Current device Id: {device.Id} with OwnId: {device.OwnId}");
+
+            // Before assign
+            var trackDataWithoutAssign = TelematicsApi.Telematics.ReadCoordinates(new ReadCoordinatesParams(device.DeviceId));
+            Console.WriteLine($"Count coordinates without pipeLine: {trackDataWithoutAssign.Count()}");
+
+            TelematicsApi.Telematics.AssignDefaultCoordinatesPreFiltersPipeline(pipeLineName);
+
+            // After assign
+            var trackDataWithAssign = TelematicsApi.Telematics.ReadCoordinates(new ReadCoordinatesParams(device.DeviceId));
+            Console.WriteLine($"Count coordinates without pipeLine: {trackDataWithAssign.Count()}");
+
+            Console.WriteLine("Press any key to exit...");
+*/
