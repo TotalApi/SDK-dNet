e64202d5c3c69922dedc2d6fba03b944d0c7661d
LionSoft <eleybov@gmail.com>
2016-09-15 16:07:07 +0300

    Add EmptyTemplate Project

---
+++ samples/EmptyTemplate/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Reflection;
+using TotalApi.Core;
+using TotalApi.Core.Api;
+using TotalApi.Core.Api.ControlApi;
+using TotalApi.Core.Authentication;
+using TotalApi.Core.Events;
+using TotalApi.Utils;
+using TotalApi.Utils.Console;
+using TotalApi.Utils.ErrorManager;
+
+namespace EmptyTemplate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                ColorConsole.Do(ConsoleColor.Yellow, () => Console.WriteLine("-- Loading modules --\r\n"));
+
+                // Initialization client SDK
+                TotalApiBootstrapper.Create();
+
+                Console.Title = Params.Get<string>("appTitle") ?? Params.Get<string>("appId") ?? Assembly.GetExecutingAssembly().GetName().Name;
+
+                ColorConsole.Do(ConsoleColor.Yellow, () => Console.WriteLine("-- Modules are loaded --\r\n"));
+
+                CoreApi.Logger.InfoInstance(TotalApiEventManagerBase.LogInstance, "CLIENT_ID: {0}", TotalApiAuth.ClientId);
+
+                //
+                // Write your code here
+                //
+
+                ConsoleKeyHandlersManager.StartListeningConsole();
+            }
+            catch (Exception e)
+            {
+                ColorConsole.Do(ConsoleColor.Red, ()=> Console.WriteLine(e.FullMessage()));
+                Console.WriteLine("Press any key to exit...");
+                Console.ReadKey();
+            }
+        }
+    }
+}
